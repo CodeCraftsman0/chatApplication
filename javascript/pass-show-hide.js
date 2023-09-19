a2eee571be8834dcf1b6aca78d034f6c59d2b1bd
Saurabh <144561324+CodeCraftsman0@users.noreply.github.com>
2023-09-19 17:52:05 +0530

    Add files via upload

---
+++ javascript/pass-show-hide.js
@@ -0,0 +1,21 @@
+
+// PASSWORD SHOW OR HIDE PART START HERE 
+
+const pswrdField = document.querySelector(".form input[type='password']");
+toggleBtn = document.querySelector(".form .field i");
+
+toggleBtn.onclick = ()=>{
+    // alert();
+
+    if(pswrdField.type == "password"){
+        pswrdField.type = "text";
+        toggleBtn.classList.add("active");
+    }
+
+    else{
+        pswrdField.type = "password";
+        toggleBtn.classList.remove("active");
+    }
+}
+
+// PASSWORD SHOW OR HIDE PART CLOSE HERE 
